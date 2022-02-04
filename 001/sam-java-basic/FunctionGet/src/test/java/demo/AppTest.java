0c4272ef07f1e661a93a9cd793d1fbf69c9aac65
christoferson <sweetsorrow1981@yahoo.co.jp>
2022-02-04 11:15:10 +0900

    Add Test

---
+++ 001/sam-java-basic/FunctionGet/src/test/java/demo/AppTest.java
@@ -0,0 +1,25 @@
+package demo;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+import demo.App;
+
+public class AppTest {
+  @Test
+  public void successfulResponse() {
+    App app = new App();
+    APIGatewayProxyResponseEvent result = app.handleRequest(null, null);
+    assertEquals(200, result.getStatusCode().intValue());
+    assertEquals("application/json", result.getHeaders().get("Content-Type"));
+    String content = result.getBody();
+    assertNotNull(content);
+    assertTrue(content.contains("\"message\""));
+    assertTrue(content.contains("\"83928392818\""));
+    assertTrue(content.contains("\"location\""));
+    assertTrue(content.contains("\"timestamp\""));
+  }
+}
